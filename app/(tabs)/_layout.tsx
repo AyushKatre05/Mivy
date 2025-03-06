6765278d46306925496efe57bebda4d150aa66df
Ayush Katre <ayushkatre1801@gmail.com>
2025-03-06 16:17:43 +0530

    added icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,57 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
+import { View, Text, Image } from "react-native";
+import React, { useContext } from "react";
+import { Tabs } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { AuthContext } from "@/context/Authcontext";
+import Colors from "@/data/Colors";
 
 const TabLayout = () => {
+  const { user } = useContext(AuthContext);
   return (
-    <Tabs>
-        <Tabs.Screen name='Home' />
-        <Tabs.Screen name='Event' />
-        <Tabs.Screen name='Clubs' />
-        <Tabs.Screen name='Profile' />
+    <Tabs screenOptions={{
+        tabBarActiveTintColor:Colors.PRIMARY,
+        headerShown:false
+    }}>
+      <Tabs.Screen
+        name="Home"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Event"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="calendar-sharp" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Clubs"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="people" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Profile"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Image source={{ uri: user?.image }} style={
+                {
+                    width:size,
+                    height:size,
+                    borderRadius:99
+                }
+            } />
+          ),
+        }}
+      />
     </Tabs>
-  )
-}
+  );
+};
 
-export default TabLayout
+export default TabLayout;
