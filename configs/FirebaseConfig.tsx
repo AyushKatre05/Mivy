53d1132e4054befc74032065d01fe5b0eb27792d
Ayush Katre <ayushkatre1801@gmail.com>
2025-03-06 15:01:07 +0530

    firebase for auth

---
+++ configs/FirebaseConfig.tsx
@@ -0,0 +1,30 @@
+import { initializeApp } from 'firebase/app';
+//@ts-ignore
+import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDHNVt5T9BLQzKUm98cUN0_W2SlFOFxhMw",
+    authDomain: "mivy-4114c.firebaseapp.com",
+    projectId: "mivy-4114c",
+    storageBucket: "mivy-4114c.firebasestorage.app",
+    messagingSenderId: "140274485453",
+    appId: "1:140274485453:web:54618ec20a6e9abb984184",
+    measurementId: "G-95JHWXN6E8"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Auth with React Native persistence
+const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage),
+});
+
+export { auth };
+
+// const analytics = getAnalytics(app);
