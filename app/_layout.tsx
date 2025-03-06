ffe3ec26b8bfcba047cce34cc0b4e8b5339abc82
Ayush Katre <ayushkatre1801@gmail.com>
2025-03-06 15:02:18 +0530

    changes done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,13 @@
 import { Stack } from "expo-router";
+import {AuthContext} from "@/context/Authcontext"
+import { useState } from "react";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [user, setUser] = useState(null);
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+    <Stack screenOptions={{ headerShown: false }}>
+    </Stack>
+    </AuthContext.Provider>
+  )
 }
